19668a3c7147d41c8bd3dc4d321a4f218df4ef2d
مهدیار اورعی <mahdiyar.oraei@gmial.com>
2021-09-14 13:08:43 +0600

    Fix product widget

--- lib/module/home/screen/component/product_widget.dart
+++ lib/module/home/screen/component/product_widget.dart
@@ -16,6 +16,49 @@ class ProductWidget extends StatelessWidget {
           color: Colors.white,
           borderRadius: BorderRadius.circular(15)
         ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12),
+          child: Column(
+            children: [
+              Image.asset("res/Images/product_charger_wire.jpg"),
+              SizedBox(height: 7,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("سیم شارژر موبایل اندرویدی"),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12.0),
+                child: Text("۳،۶۰۰ تومان هدیه نقدی", style: Theme.of(context).textTheme.caption,),
+              ),
+              Expanded(child: Center()),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(18)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 10),
+                      child: Text("٪ ۳۷", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                    ),
+                  ),
+                  Expanded(child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text("۳۸۹،۰۰۰ تومان", textAlign: TextAlign.end,),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 36),
+                        child: Text("۴۸۹،۰۰۰", textAlign: TextAlign.end, style: Theme.of(context).textTheme.caption?.copyWith(decoration: TextDecoration.lineThrough),),
+                      ),
+                    ],
+                  )),
+                ],
+              ),
+              SizedBox(height: 12,)
+            ],
+          ),
+        ),
       ),
     );
   }
